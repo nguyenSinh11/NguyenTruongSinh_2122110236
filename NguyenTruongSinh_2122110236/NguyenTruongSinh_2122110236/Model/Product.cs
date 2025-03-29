c39a5638ac2b75b65c75d580d6516c922149ffa9
Sinh Nguyễn Trường <gaupanda.edu@gmail.com>
2025-03-29 08:30:29 +0700

    Add files via upload

---
+++ NguyenTruongSinh_2122110236/NguyenTruongSinh_2122110236/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace NguyenTruongSinh_2122110236.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Image { get;set; }
+    }
+}
